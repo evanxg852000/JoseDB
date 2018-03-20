4ac7c541a947cc005b768ffb73b38b01e18467f2
Evance Soumaoro <evanxg852000@yahoo.fr>
2018-03-20 19:10:25 +0000

    initial project setup

---
+++ src/main/java/io/josedb/Program.java
@@ -0,0 +1,12 @@
+
+package io.josedb;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello from JoseDB");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/io/josedb/test/ProgramTest.java
@@ -0,0 +1,14 @@
+package io.josedb.test;
+
+import org.junit.Test;
+
+import io.josedb.Program;
+
+import static org.junit.Assert.*;
+
+public class ProgramTest {
+    @Test public void testSomeLibraryMethod() {
+        Program classUnderTest = new Program();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
